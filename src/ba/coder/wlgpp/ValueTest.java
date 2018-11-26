8c2772b979c96aa6ea7603031aeb2bbbb65f4a3d
wlgpp <wlgpp@wlgpp>
2018-11-26 21:54:19 +0900

    init

---
+++ src/ba/coder/wlgpp/ValueTest.java
@@ -0,0 +1,28 @@
+package ba.coder.wlgpp;
+
+public class ValueTest {
+	int globalVariable = 5;
+
+	public static void main(String[] args) {
+
+		int aVal = 10;
+		int bVal = 5;
+		int Tot;
+		
+		Tot = aVal + bVal;
+		System.out.println(Tot);
+			
+		}
+	
+	public void add() {
+		int val2=5;
+		int total = globalVariable + val2;
+		
+		/*
+		Tot  =aVal+bVal;
+		System.out.println("Total = " +Tot);
+		*/
+		
+	}
+
+}
