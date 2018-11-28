0783f4ef8aff05c1bfcde15719115a1a95f949fd
wlgpp <wlgpp@wlgpp>
2018-11-28 22:07:36 +0900

    day2

---
+++ src/ba/coder/wlgpp/day2/HomeWork.java
@@ -0,0 +1,15 @@
+package ba.coder.wlgpp.day2;
+
+public class HomeWork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int aVal = InputVal.getInt();
+		System.out.println("aVal : " + aVal);
+		
+		double bVal = InputVal.getDouble();
+		System.out.println("bVal : "+ bVal);
+		
+	}
+
+}
---
+++ src/ba/coder/wlgpp/day2/InputVal.java
@@ -0,0 +1,22 @@
+package ba.coder.wlgpp.day2;
+
+import javax.swing.JOptionPane;
+
+public class InputVal {
+	public static int getInt() {
+		String input = JOptionPane.showInputDialog("정수 입력");
+		int result = Integer.parseInt(input);
+		return result;
+		
+	}
+	
+	public static double getDouble() {
+		String input = JOptionPane.showInputDialog("실수 입력");
+		double result = Double.parseDouble(input);
+		return result;
+	}
+	
+	
+	
+
+}
